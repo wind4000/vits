38ec1ef6dfffa99a16fed6bae2efae5aa4870bf2
wind4000 <109077979+wind4000@users.noreply.github.com>
2022-10-22 12:24:00 +0800

    cherry pick english cleaners from CjangCjengh/vits

--- text/cleaners.py
+++ text/cleaners.py
@@ -19,6 +19,10 @@ from jamo import h2j, j2hcj
 from pypinyin import lazy_pinyin, BOPOMOFO
 import jieba, cn2an
 
+# Separated from mixed cleaners
+from english import english_to_ipa as english_cleaners_ipa
+from english import english_to_ipa2 as english_cleaners_ipa2
+from english import english_to_lazy_ipa as english_cleaners_lazy_ipa
 
 # This is a list of Korean classifiers preceded by pure Korean numerals.
 _korean_classifiers = '군데 권 개 그루 닢 대 두 마리 모 모금 뭇 발 발짝 방 번 벌 보루 살 수 술 시 쌈 움큼 정 짝 채 척 첩 축 켤레 톨 통'
